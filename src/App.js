96971c14e278d17276ccb1254ac6999b71271d33
dkdkwl <dkdkwl@naver.com>
2022-11-14 16:26:02 +0900

    card 컴포넌트화 만들기

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import data from './data.js';
 
 function App(){
 
-  let [shoes] = useState();
+  let [shoes] = useState(data);
 
   return (
     <div className="App">
@@ -22,24 +22,27 @@ function App(){
       </Navbar>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            <h4>{data[0].title}</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
+          {
+            shoes.map(function(a,i){
+              return(
+                <Card shoes={shoes[i]} key = {i}/>
+              )
+            })
+          }
         </div>
       </div>
     </div>
+  )
+}
+
+function Card(props){
+  return(
+  <div className="col-md-4" >
+    <img src={"https://codingapple1.github.io/shop/shoes1.jpg"} width="80%" />
+    <h4>{props.shoes.title}</h4>
+    <p>{props.shoes.content}</p>
+    <p>{props.shoes.price}</p>
+  </div>
   )
 }
 
